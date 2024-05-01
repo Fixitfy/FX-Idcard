8d3dae2851fbcc4668e5e0219c86e9da7de78209
Fixitfy <139653962+Fixitfy@users.noreply.github.com>
2024-05-01 13:36:07 +0300

    Add files via upload

---
+++ s/opensource.lua
@@ -0,0 +1,33 @@
+RegisterCommand(Config.DeletePlayerDataCommand,function(source,args)
+    if args and args[1] then
+        local src = source
+        local Character = FXGetPlayerData(src)
+        if Character.admin or Character.job == "judge" then
+            local target = FXGetPlayerData(tonumber(args[1]))
+            MySQL.query.await('DELETE FROM `fx_idcard` WHERE charid = ?', {target.charIdentifier})
+            TriggerClientEvent('fx-idcard:client:updateData',tonumber(args[1]))
+            Notify({
+                source = source,
+                text = Locale("successdelete"),
+                type = "success",
+                time = 4000
+            })
+        else
+            Notify({
+                source = source,
+                text = Locale("nojob"),
+                type = "error",
+                time = 4000
+            })
+
+
+        end
+    else
+        Notify({
+            source = source,
+            text = Locale("errorcommand"),
+            type = "error",
+            time = 4000
+        })
+    end
+end)
