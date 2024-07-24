6399706afd5278eac7d5c231de06251232772233
Fixitfy <139653962+Fixitfy@users.noreply.github.com>
2024-07-24 21:43:50 +0300

    Add files via upload

---
+++ frameworks.lua
@@ -0,0 +1,229 @@
+if Config.Framework == "VORP" then
+    if IsDuplicityVersion() then
+        --[[
+            Server Side
+        ]]
+        local VorpCore = exports.vorp_core:GetCore()
+    
+        VorpInv = {}
+        VorpInv = exports.vorp_inventory:vorp_inventoryApi()
+        
+        function FXRegisterUsableItem(itemname,callBack)
+            exports.vorp_inventory:registerUsableItem(itemname, function(data)
+                local array = {
+                    source = data.source,
+                    item = data.item
+                }
+                callBack(array)
+            end)
+        end
+        
+        function FXCloseInventory(src)
+            exports.vorp_inventory:closeInventory(src)
+        end
+        
+        function FXRemoveItem(src,itemName,itemCount,Metadata)
+            return exports.vorp_inventory:subItem(src, itemName, itemCount, Metadata)
+        end
+        
+        function FXAddItem(src,itemName,itemCount,Metadata)
+            return exports.vorp_inventory:addItem(src, itemName, itemCount, Metadata)
+        end
+
+        function FXHaveMoney(src,moneytype,count)
+            local User = VorpCore.getUser(src)
+            local Character = User.getUsedCharacter
+            if Character.money >= count and moneytype == "cash" then
+                return true
+            elseif Character.gold >= count and moneytype == "gold" then
+                return true
+            end
+            return false
+        end
+        function FXRemoveMoney(src,moneytype,count)
+            local User = VorpCore.getUser(src)
+            local Character = User.getUsedCharacter
+            local vorpmoneytype = moneytype == "gold" and 1 or 0
+            Character.removeCurrency(vorpmoneytype, count)
+            return true
+        end
+        function FXAddMoney(src,moneytype,count)
+            local User = VorpCore.getUser(src)
+            local Character = User.getUsedCharacter
+            local vorpmoneytype = moneytype == "gold" and 1 or 0
+            Character.addCurrency(vorpmoneytype, count)
+            return true
+        end
+    
+        function FXGetCharacterInformations(src, charid,cb)
+            local array = {}
+            exports.oxmysql:execute("SELECT * FROM characters WHERE charidentifier = ?", {charid},function(result)
+                if result[1] then
+                    array.height = json.decode(result[1].skinPlayer).Scale
+                    array.sex = result[1].gender
+                    array.weight = math.random(45,65)
+                    if array.sex == "Male" then
+                        array.weight = math.random(70,100)
+                    end
+                    array.firstname = result[1].firstname
+                    array.lastname = result[1].lastname
+                    array.age = result[1].age
+                end
+                cb(array)
+            end)
+        end
+        
+        function FXGetPlayerData(src)
+            local User = VorpCore.getUser(src)
+            local Character = User.getUsedCharacter
+            local array = {
+                firstname = Character.firstname,
+                lastname = Character.lastname,
+                charIdentifier = Character.charIdentifier,
+                cash = Character.money,
+                gold = Character.gold,
+                admin = User.getGroup == "admin"
+            }
+            return array
+        end
+    
+    else
+    --[[
+        Client Side
+    ]]    
+    
+        VorpCore = exports.vorp_core:GetCore()
+        
+        RegisterNetEvent('vorp:SelectedCharacter',function()
+            TriggerServerEvent('fx-idcard:server:GetData')
+        end)
+        
+    end
+elseif Config.Framework == "RSG" then
+    if IsDuplicityVersion() then
+        --[[
+            Server Side
+        ]]
+        RSGCore = exports['rsg-core']:GetCoreObject()
+        function FXRegisterUsableItem(itemname,callBack)
+            RSGCore.Functions.CreateUseableItem(itemname, function(source,item)
+                local array = {
+                    source = source,
+                    item = item
+                }
+                array.item.item = item.name
+                array.item.metadata = item.info
+                callBack(array)
+            end)
+        end
+        
+        function FXCloseInventory(src)
+            TriggerClientEvent("rsg-inventory:client:closeinv", src)
+        end
+        
+        function FXRemoveItem(src,itemName,itemCount,Metadata)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            local foundslot = false
+            for slot,item in pairs(Player.PlayerData.items) do
+                if item.name:lower() == itemName:lower() then
+                    if Metadata then
+                        if item.info.privateID == Metadata.privateID then
+                            foundslot = slot
+                            break
+                        end
+                    else
+                        foundslot = slot
+                        break
+                    end
+                end
+            end
+            if not foundslot then return false end
+            Player.Functions.RemoveItem(itemName, itemCount,foundslot)
+        end
+        
+        function FXAddItem(src,itemName,itemCount,Metadata)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            Player.Functions.AddItem(itemName, itemCount,false,Metadata)
+        end
+        function FXHaveMoney(src,moneytype,count)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            local pcash = Player.PlayerData.money['cash']
+            local pbank = Player.PlayerData.money['bank']
+            if pcash >= count and moneytype == "cash" then
+                return true
+            else
+                if pbank >= count and moneytype == "gold" then
+                    return true
+                end
+            end
+            return false
+        end
+        function FXRemoveMoney(src,moneytype,count)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            if moneytype == "cash" then
+                Player.Functions.RemoveMoney('cash', count, 'fx-idcard')
+            else
+                Player.Functions.RemoveMoney('bank', count, 'fx-idcard')
+            end
+            return true
+        end
+        function FXAddMoney(src,moneytype,count)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            if moneytype == "cash" then
+                Player.Functions.AddMoney('cash', count, 'fx-idcard')
+            else
+                Player.Functions.AddMoney('bank', count, 'fx-idcard')
+            end
+            return true
+        end
+        ---- RSG UYARLANACAK --- SQL = players.charinfo and playerskins
+        function FXGetCharacterInformations(src, charid,cb)
+            local array = {}
+            local Player = RSGCore.Functions.GetPlayer(src)
+
+            exports.oxmysql:execute("SELECT * FROM playerskins WHERE citizenid = ?", {charid},function(result)
+                if result[1] then
+                    local height = json.decode(result[1].skin) and json.decode(result[1].skin).height or 100
+                    array.height = math.floor(tonumber(height) / 100)
+                    array.sex = Player.PlayerData.charinfo.gender == 1 and "Female" or "Male"
+                    array.weight = math.random(45,65)
+                    if array.sex == "Male" then
+                        array.weight = math.random(70,100)
+                    end
+                    array.firstname = Player.PlayerData.charinfo.firstname
+                    array.lastname = Player.PlayerData.charinfo.lastname
+                    local year = string.sub(Player.PlayerData.charinfo.birthdate,1,4)
+             
+                    array.age = 1899 - tonumber(year)
+                end
+                cb(array)
+            end)
+        end
+        
+        function FXGetPlayerData(src)
+            local Player = RSGCore.Functions.GetPlayer(src)
+            local array = {
+                firstname = Player.PlayerData.charinfo.firstname,
+                lastname = Player.PlayerData.charinfo.lastname,
+                job = Player.PlayerData.job.name,
+                grade = Player.PlayerData.job.grade.level,
+                charIdentifier = Player.PlayerData.citizenid,
+                cash = Player.PlayerData.money["cash"],
+                gold = Player.PlayerData.money["bank"],
+                admin = RSGCore.Functions.HasPermission(src, 'admin') or IsPlayerAceAllowed(src, 'command') 
+            }
+            return array
+        end
+    else
+    --[[
+        Client Side
+    ]]    
+        RSGCore = exports['rsg-core']:GetCoreObject()
+    
+        RegisterNetEvent('RSGCore:Client:OnPlayerLoaded',function()
+            TriggerServerEvent('fx-idcard:server:GetData')
+        end)
+    end    
+end
+
+
---
+++ s.lua
@@ -0,0 +1,212 @@
+local vesikalikPlease = {}
+
+RegisterNetEvent("fx-idcard:server:print", function(link)
+    local src = source
+    if Config.Prices.printphoto then
+        local HaveMoney = FXHaveMoney(src, "cash", Config.Prices.printphoto)
+        if HaveMoney then
+            FXRemoveMoney(src,"cash",Config.Prices.printphoto)
+        else
+            return Notify({
+                source = src,
+                text = Locale("nomoney", {money=Config.Prices.printphoto}),
+                type = "success",
+                time = 4000
+            })
+        end
+    end
+    local metadata = {
+        description = Locale("photodesc").."</br>"..link,
+        img = link
+    }
+
+    FXAddItem(src, Config.PrintPhotoItem, 1, metadata)
+    Notify({
+        source = src,
+        text = Locale("successprint"),
+        type = "success",
+        time = 4000
+    })
+end)
+
+RegisterNetEvent('fx-idcard:server:GetData', function()
+    local src = source
+    local Character = FXGetPlayerData(src)
+    local charid = Character.charIdentifier
+    exports.oxmysql:execute("SELECT * FROM fx_idcard WHERE charid = ?", {charid},function(result)
+        if result[1] then
+            TriggerClientEvent('fx-idcard:client:setData',src,json.decode(result[1].data))
+        end
+    end)
+end)
+
+
+RegisterNetEvent('fx-idcard:server:useImagePlease',function(city)
+    vesikalikPlease[tostring(source)] = city
+    SetTimeout(Config.SelectPhotoTime * 1000,function()
+        vesikalikPlease[tostring(source)] = nil
+    end)
+end)
+
+RegisterNetEvent('fx-idcard:server:setBucket',function(bucket)
+    SetPlayerRoutingBucket(source,bucket)
+end)
+RegisterNetEvent("fx-idcard:server:ShowUi", function(targets, typee, data)
+    local src = source
+    if typee == "photo" then
+        TriggerClientEvent("fx-idcard:client:PreviewPhoto", src,typee, data)
+        for k,v in ipairs(targets) do
+            TriggerClientEvent("fx-idcard:client:PreviewPhoto", v,typee, data)
+        end
+    else
+        TriggerClientEvent("fx-idcard:client:PreviewPhoto", src,typee, data)
+        for k,v in ipairs(targets) do
+            TriggerClientEvent("fx-idcard:client:PreviewPhoto", v,typee, data)
+        end
+    end
+end)
+
+FXRegisterUsableItem(Config.PrintPhotoItem,function(data)
+    local src = data.source
+    local link = data.item.metadata.img
+    local data = {img = link}
+    FXCloseInventory(src)
+    if vesikalikPlease[tostring(src)] then
+        local city = vesikalikPlease[tostring(src)]
+        vesikalikPlease[tostring(src)] = nil
+        local Character = FXGetPlayerData(src)
+        FXGetCharacterInformations(src, Character.charIdentifier,function(data)
+            data = {
+                name= data.firstname.." "..data.lastname,
+                age = data.age,
+                sex = data.sex,
+                charid=Character.charIdentifier,
+                height = data.height,
+                weight = data.weight,
+                city = city,
+                img = link,
+                religious = Config.Religious[math.random(1,#Config.Religious)],
+            }
+            Wait(1000)
+            TriggerClientEvent("fx-idcard:client:CreateIdcardUi", src, data)
+        end)
+    else
+        TriggerClientEvent("fx-idcard:client:ShowUi", src, "photo", data)
+    end
+end)
+
+FXRegisterUsableItem(Config.ManIdCardItem,function(data)
+    local src = data.source
+    local itemData = data.item.metadata
+    FXCloseInventory(src)
+    TriggerClientEvent("fx-idcard:client:ShowUi", src, "idcard", itemData.CardData)
+end)
+FXRegisterUsableItem(Config.WomanIdCardItem,function(data)
+    local src = data.source
+    local itemData = data.item.metadata
+    FXCloseInventory(src)
+    TriggerClientEvent("fx-idcard:client:ShowUi", src, "idcard", itemData.CardData)
+end)
+
+RegisterNetEvent("fx-idcard:server:ShowIdCard", function(data)
+    local src = source
+    TriggerClientEvent("fx-idcard:client:ShowUi", src, "idcard", data)
+end)
+
+-- ### Data
+-- name, cityname, religious, age, height, weight, hair, eye, sex, img
+RegisterNetEvent('fx-idcard:server:buyIdCard',function(data)
+    local src = source
+    local Character = FXGetPlayerData(src)
+    if Config.TakeCardType == "sql" then
+        local charid = Character.charIdentifier
+        data.charid = charid
+        if Config.Prices.idcard then
+            local HaveMoney = FXHaveMoney(src, "cash", Config.Prices.idcard)
+            if HaveMoney then
+                FXRemoveMoney(src,"cash",Config.Prices.idcard)
+            else
+                return Notify({
+                    source = src,
+                    text = Locale("nomoney", {money=Config.Prices.idcard}),
+                    type = "success",
+                    time = 4000
+                })
+            end
+        end
+        exports.oxmysql:execute("SELECT * FROM fx_idcard WHERE charid = ?", {charid},function(result)
+            if not result[1] then
+                local Parameters = {
+                    ['charid'] = charid,
+                    ['data'] = tostring(json.encode(data)),
+                }
+                exports.oxmysql:execute("INSERT INTO fx_idcard (`charid`, `data`) VALUES (@charid, @data)", Parameters)
+                TriggerClientEvent('fx-idcard:client:setData',src,data)
+                Notify({
+                    source = src,
+                    text = Locale("successidcard"),
+                    type = "success",
+                    time = 4000
+                })
+                CharData = data
+            else
+                Notify({
+                    source = src,
+                    text = Locale("alreadyidcard"),
+                    type = "error",
+                    time = 4000
+                })
+            end
+        end)
+    elseif Config.TakeCardType == "item" then
+        local item = Config.ManIdCardItem
+        local metadata = {
+            description = Locale("idcarddesc",{
+                name=data.name,
+                age = Character.age,
+                charid=Character.charIdentifier,
+            }),
+            -- name, cityname, religious, age, date, height, weight, hair, eye, sex, img
+            CardData = {
+                name=data.name,
+                cityname=data.cityname,
+                religious = data.religious,
+                age = data.age,
+                date = data.date,
+                height = data.height,
+                weight = data.weight,
+                hair = data.hair,
+                eye = data.eye,
+                sex = data.sex,
+                charid = Character.charIdentifier,
+                img = data.img,
+            }
+        }
+        if data.sex == "Female" then
+            item = Config.WomanIdCardItem
+        end
+        if Config.Prices.idcard then
+            local HaveMoney = FXHaveMoney(src, "cash", Config.Prices.idcard)
+            if HaveMoney then
+                FXRemoveMoney(src,"cash",Config.Prices.idcard)
+            else
+                return Notify({
+                    source = src,
+                    text = Locale("nomoney", {money=Config.Prices.idcard}),
+                    type = "success",
+                    time = 4000
+                })
+            end
+        end
+        
+        FXAddItem(src, item, 1, metadata)
+
+        Notify({
+            source = src,
+            text = Locale("addIdCard"),
+            type = "success",
+            time = 4000
+        })
+        
+    end
+end)
